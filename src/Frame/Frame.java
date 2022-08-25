9abbf522b5c9885625a7de2858b4b325a6df1fb1
k-isaka <isk.rugby@gmail.com>
2022-08-25 23:02:08 +0900

    comment

--- src/Frame/Frame.java
+++ src/Frame/Frame.java
@@ -5,11 +5,13 @@ import javax.swing.JFrame;
 
 public class Frame{
     public static void main(String[] args) {
+//        ビデオ再生用ウィンドウ
         JFrame videoFrame = new JFrame("Rugby Coder");
         videoFrame.setTitle("Rugby Coder");
         videoFrame.setVisible(true);
         videoFrame.setSize(700,450);
 
+//        タイムライン用ウィンドウ
         JFrame tlFrame = new JFrame("Timeline");
         tlFrame.setTitle("TimeLine");
         tlFrame.setVisible(true);
