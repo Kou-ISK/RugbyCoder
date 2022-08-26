8ecf86640456ca4d50a2601ddc626f2486a68508
k-isaka <isk.rugby@gmail.com>
2022-08-27 00:59:05 +0900

    JButtonの実装

--- src/Frame/Frame.java
+++ src/Frame/Frame.java
@@ -1,6 +1,8 @@
 package Frame;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.time.LocalTime;
 
 
 public class Frame{
@@ -17,5 +19,44 @@ public class Frame{
         tlFrame.setVisible(true);
         tlFrame.setSize(1400, 400);
         tlFrame.setLocation(0,490);
+
+//        コードウィンドウ
+        JFrame codeWindow = new JFrame("Code Window");
+        codeWindow.setLayout(new FlowLayout());
+        codeWindow.setTitle("Code Window");
+        codeWindow.setVisible(true);
+        codeWindow.setSize(500,600);
+        codeWindow.setLocation(750,0);
+        JButton startButton = new JButton("Start");
+        JButton endButton = new JButton("End");
+        JButton tackleButton = new JButton("Tackle");
+        JButton scrumButton = new JButton("Scrum");
+        JButton lineOutButton = new JButton("Line Out");
+        startButton.setSize(400,100);
+        endButton.setSize(400,100);
+        tackleButton.setSize(400,200);
+        scrumButton.setSize(400,200);
+        lineOutButton.setSize(400,200);
+
+        LocalTime startTime;
+        LocalTime endTime;
+
+        tackleButton.addActionListener(e -> {
+            System.out.println("tackle");
+//            System.out.println(startTime);
+//            System.out.println(endTime);
+        });
+
+        scrumButton.addActionListener(e -> System.out.println("scrum"));
+
+        Container cwContainer = codeWindow.getContentPane();
+        cwContainer.add(startButton);
+        cwContainer.add(endButton);
+        cwContainer.add(tackleButton);
+        cwContainer.add(scrumButton);
+        cwContainer.add(lineOutButton);
+
+        cwContainer.setVisible(true);
+
     }
 }
---
+++ src/Logic/Logic.java
@@ -0,0 +1,10 @@
+package Logic;
+
+import java.util.Timer;
+
+public class Logic {
+    private Timer timer;
+
+    public void timelineStart(){
+    }
+}
