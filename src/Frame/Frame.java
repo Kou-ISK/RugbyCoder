63207d0c96d2fd0f1072eab055a5acbd5a71a7f8
k-isaka <isk.rugby@gmail.com>
2022-09-09 23:52:29 +0900

    sliderでseek(自動では動かない)

--- src/Frame/Frame.java
+++ src/Frame/Frame.java
@@ -1,9 +1,13 @@
 package Frame;
 
 import DataObject.DataObject;
+import DataObject.timeObject;
 import Logic.Logic;
+import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.JFXPanel;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -18,16 +22,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
-public class Frame {
+import static java.util.Objects.isNull;
 
+public class Frame {
     public static void main(String[] args) throws Exception {
         String mediaName = "mediaName";
-        String filePath = "/Users/isakakou/Desktop/MAH00202.MP4";
+        String filePath = "/Users/isakakou/Desktop/ドラゴンクエストⅤ 序曲のマーチ.mp4";
         String fileName;
 //      Creating Media Player Window
         window videoWindow = new window("Rugby Coder", 700, 450);
         //Input Video File Path
         MoviePanel mp = new MoviePanel(filePath);
+
         Logic logic = new Logic();
         logic.setName(mediaName);
         csvViewer csvViewer = new csvViewer(mediaName);
@@ -35,6 +41,7 @@ public class Frame {
         //JavaFX動画インスタンスとプレイヤーを取得
         Media media = mp.getMedia();
         MediaPlayer player = mp.getPlayer();
+
         codeWindow cWindow = new codeWindow(logic, "Code Window", player, 500, 800);
         //Loading
         for (int i = 0; player.getStatus() != MediaPlayer.Status.READY; i++) {
@@ -82,11 +89,35 @@ public class Frame {
 
         //動画の再生
 //指定した時間へとジャンプ
-        player.seek(logic.getDataFromCsv("00:01:00"));
+        Slider s = mp.getSlider();
+        timeObject to = new timeObject();
+        s.valueProperty().addListener((
+                ObservableValue<? extends Number> ov,
+                Number old_val, Number new_val) -> {
+            to.setNow(new_val.intValue());
+            System.out.println(to.getNow());
+            int now = to.getNow();
+            if (isNull(now)) {
+                player.seek(logic.getDataFromCsv("00:00:00"));
+            } else {
+                int rawTime = to.getNow();
+                int second = rawTime % 60;
+                int minute = ((rawTime % 3600) / 60);
+                int hour = rawTime / 3600;
+                String time = String.format("%02d:%02d:%02d",
+                        hour, minute, second);
+                player.seek(logic.getDataFromCsv(time));
+
+            }
+        });
+
+
         player.play();
         System.out.println("Current: " + player.getCurrentTime());
         System.out.println(player.getStopTime());
     }
+
+
 }
 
 
@@ -138,7 +169,13 @@ class button extends JButton {
 class MoviePanel extends JFXPanel {
     private Media media;
     private MediaPlayer player;
+    private int sliderTime;
+    private Slider slider;
 
+    int getSliderTime() {
+        this.sliderTime = (int) slider.getValue();
+        return this.sliderTime;
+    }
 
     MoviePanel(String filePath) {
 
@@ -152,7 +189,14 @@ class MoviePanel extends JFXPanel {
         media = new Media(f.toURI().toString());
         player = new MediaPlayer(media);
         MediaView mediaView = new MediaView(player);
-        root.getChildren().add(mediaView);
+        root.getChildren().add(0, mediaView);
+        int totalTime = (int) player.getTotalDuration().toSeconds();
+        slider = new Slider(0, totalTime, 0);
+        slider.increment();
+        int sliderTime = (int) slider.getValue();
+        root.getChildren().add(1, slider);
+        root.setAlignment(slider, Pos.BOTTOM_CENTER);
+
 
         int rawTime = (int) media.getDuration().toSeconds();
         int second = rawTime % 60;
@@ -165,10 +209,10 @@ class MoviePanel extends JFXPanel {
         System.out.println(time);
         //JavaFXScene
         Scene scene = new Scene(root);
-
         //JFXPanelにSceneをセット
         setScene(scene);
     }
+
 
     Media getMedia() {
         return media;
@@ -178,7 +222,9 @@ class MoviePanel extends JFXPanel {
         return player;
     }
 
-
+    Slider getSlider() {
+        return slider;
+    }
 }
 
 class csvViewer extends JFrame {
